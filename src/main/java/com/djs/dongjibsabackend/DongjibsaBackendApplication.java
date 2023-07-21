b48e5d115eb8b2a5e41a7b5d123c8f5b9069a94f
hyeonju01 <tangled6648@gmail.com>
2023-07-21 18:06:16 +0900

    Initial commit

---
+++ src/main/java/com/djs/dongjibsabackend/DongjibsaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.djs.dongjibsabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongjibsaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongjibsaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djs/dongjibsabackend/DongjibsaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djs.dongjibsabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongjibsaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
