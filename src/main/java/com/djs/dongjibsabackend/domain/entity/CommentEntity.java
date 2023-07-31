91a20a6cbb7934025e5a8b609f0da0fc473b7f98
hyeonju01 <tangled6648@gmail.com>
2023-07-31 22:00:59 +0900

    chore: id 컬럼 어노테이션 설정

--- src/main/java/com/djs/dongjibsabackend/domain/entity/CommentEntity.java
+++ src/main/java/com/djs/dongjibsabackend/domain/entity/CommentEntity.java
@@ -1,5 +1,6 @@
 package com.djs.dongjibsabackend.domain.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +20,7 @@ import org.hibernate.annotations.ColumnDefault;
 public class CommentEntity extends BaseEntity {
 
     @Id
+    @Column(name = "id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String message;
--- src/main/java/com/djs/dongjibsabackend/domain/entity/LocationEntity.java
+++ src/main/java/com/djs/dongjibsabackend/domain/entity/LocationEntity.java
@@ -1,5 +1,6 @@
 package com.djs.dongjibsabackend.domain.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,6 +14,7 @@ import lombok.Getter;
 public class LocationEntity {
 
     @Id
+    @Column(name = "id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String si;
