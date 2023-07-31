49436b196ebd3d35999dc8ece4e64637b62a9024
hyeonju01 <tangled6648@gmail.com>
2023-08-01 01:44:13 +0900

    fix: 빌드 에러를 해결하기 위해 test 어노테이션 주석 처리

--- src/test/java/com/djs/dongjibsabackend/DongjibsaBackendApplicationTests.java
+++ src/test/java/com/djs/dongjibsabackend/DongjibsaBackendApplicationTests.java
@@ -3,7 +3,8 @@ package com.djs.dongjibsabackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// 빌드 에러를 해결하기 위해 어노테이션 주석 처리
+//@SpringBootTest
 class DongjibsaBackendApplicationTests {
 
 	@Test
