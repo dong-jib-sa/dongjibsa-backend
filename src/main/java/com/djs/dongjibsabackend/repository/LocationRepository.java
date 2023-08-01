a8be1bcdc6c3dd44cb0a54d2a1a54ad96fd22826
hyeonju01 <tangled6648@gmail.com>
2023-08-01 22:58:26 +0900

    feat: 레시피 게시글 작성에 필요한 Location, Recipe, User Repository 인터페이스 작성

---
+++ src/main/java/com/djs/dongjibsabackend/repository/LocationRepository.java
@@ -0,0 +1,11 @@
+package com.djs.dongjibsabackend.repository;
+
+import com.djs.dongjibsabackend.domain.entity.LocationEntity;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationRepository extends JpaRepository<LocationEntity, Long> {
+
+    @Override
+    Optional<LocationEntity> findById(Long id);
+}
