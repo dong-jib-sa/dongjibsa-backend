c4f22400e5ac50bcb0f387ed79b9be92d3ca27f8
hyeonju01 <tangled6648@gmail.com>
2023-08-02 18:07:43 +0900

    feat: 레시피 작성 요청 Dto 클래스 작성

--- src/main/java/com/djs/dongjibsabackend/domain/dto/recipe/WriteRecipeRequest.java
+++ src/main/java/com/djs/dongjibsabackend/domain/dto/recipe/WriteRecipeRequest.java
@@ -1,9 +1,13 @@
 package com.djs.dongjibsabackend.domain.dto.recipe;
 
+import com.djs.dongjibsabackend.domain.dto.ingredient.IngredientDto;
+import com.djs.dongjibsabackend.domain.dto.ingredient.IngredientResponse;
 import com.djs.dongjibsabackend.domain.entity.IngredientEntity;
 import com.djs.dongjibsabackend.domain.entity.LocationEntity;
 import com.djs.dongjibsabackend.domain.entity.RecipeEntity;
+import com.djs.dongjibsabackend.domain.entity.RecipeIngredientEntity;
 import com.djs.dongjibsabackend.domain.entity.UserEntity;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,25 +17,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WriteRecipeRequest {
 
-//    private Long id;
-    private String title;
-    private String content;
-    private Integer expectingPrice;
-    private Integer pricePerOne;
-//    private UserEntity user;
-    private Integer calorie;
-    private Integer peopleCount;
-    private LocationEntity location;
-    private IngredientEntity ingredient;
-    private Integer totalQty;
-    private Integer requiredQty;
-    private Integer sharingAvailableQty;
+    private String title; // 레시피 제목
+    private String content; // 내용
+    private Integer expectingPrice; // 예상 가격
+    private Integer pricePerOne; // 1인 당 예상 가격
+    private Integer calorie; // 1인분 칼로리
+    private Integer peopleCount; // 파티원 수
+    private LocationEntity location; // 지역
+    private List<RecipeIngredientEntity> ingredientList; // 재료 (재료명, 구매 수량, 필요 수량, 나눔 수량)
     private String imgUrl;
 
     @Builder
     public WriteRecipeRequest(String title, String content, Integer expectingPrice, Integer pricePerOne, Integer calorie,
                               Integer peopleCount,
-                              LocationEntity location, IngredientEntity ingredient, Integer totalQty, Integer requiredQty,
+                              LocationEntity location,
+                              List<RecipeIngredientEntity> ingredientList, Integer totalQty,
+                              Integer requiredQty,
                               Integer sharingAvailableQty, String imgUrl) {
         this.title = title;
         this.content = content;
@@ -40,28 +41,31 @@ public class WriteRecipeRequest {
         this.calorie = calorie;
         this.peopleCount = peopleCount;
         this.location = location;
-        this.ingredient = ingredient;
-        this.totalQty = totalQty;
-        this.requiredQty = requiredQty;
-        this.sharingAvailableQty = sharingAvailableQty;
+        this.ingredientList = ingredientList;
         this.imgUrl = imgUrl;
     }
 
     public RecipeEntity toEntity(UserEntity user, LocationEntity location) {
         return RecipeEntity.builder()
-            .title(this.title)
-            .content(this.content)
-            .expectingPrice(this.expectingPrice)
-            .pricePerOne(this.pricePerOne)
-            .user(user)
-            .calorie(this.calorie)
-            .peopleCount(this.peopleCount)
-            .location(location)
-            .ingredient(this.ingredient)
-            .totalQty(this.totalQty)
-            .requiredQty(this.requiredQty)
-            .sharingAvailableQty(this.sharingAvailableQty)
-            .imgUrl(this.imgUrl)
+                           .title(this.title)
+                           .content(this.content)
+                           .expectingPrice(this.expectingPrice)
+                           .pricePerOne(this.pricePerOne)
+                           .user(user)
+                           .calorie(this.calorie)
+                           .peopleCount(this.peopleCount)
+                           .location(location)
+                           .ingredientList(this.ingredientList)
+                           .imgUrl(this.imgUrl)
                            .build();
     }
+
+//    public List<IngredientEntity> fromDtosToEntities(List<IngredientDto> ingredientDtos) {
+//
+//        return ingredientDtos.stream().map(ingredientDto -> IngredientResponse.builder()
+//            .id(ingredientDto.getId())
+//            .ingredientName(ingredientDto.getName())
+//            .build());
+//
+//    }
 }
