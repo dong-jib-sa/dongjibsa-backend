67570cb48ab04be054b6211ff65f8f1233e286b7
hyeonju01 <tangled6648@gmail.com>
2023-08-02 22:31:33 +0900

    feat: 레시피 조회를 위한 Service 메서드 작성

--- src/main/java/com/djs/dongjibsabackend/service/RecipeService.java
+++ src/main/java/com/djs/dongjibsabackend/service/RecipeService.java
@@ -1,9 +1,11 @@
 package com.djs.dongjibsabackend.service;
 
 import com.djs.dongjibsabackend.domain.dto.recipe.RecipeDto;
+import com.djs.dongjibsabackend.domain.dto.recipe.RecipeDtoOld;
 import com.djs.dongjibsabackend.domain.dto.recipe.WriteRecipeRequest;
 import com.djs.dongjibsabackend.domain.entity.LocationEntity;
 import com.djs.dongjibsabackend.domain.entity.RecipeEntity;
+import com.djs.dongjibsabackend.domain.entity.RecipeEntityOld;
 import com.djs.dongjibsabackend.domain.entity.UserEntity;
 import com.djs.dongjibsabackend.exception.AppException;
 import com.djs.dongjibsabackend.exception.ErrorCode;
@@ -12,6 +14,8 @@ import com.djs.dongjibsabackend.repository.RecipeRepository;
 import com.djs.dongjibsabackend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,20 +28,26 @@ public class RecipeService {
     private final LocationRepository locationRepository;
 
     // 여러 가지 재료 등록 기능 추가하기
-    public RecipeDto register(WriteRecipeRequest writeRecipeRequest, UserEntity user) {
-        Long userId = user.getId();
-        // Long locationId = writeRecipeRequest.getLocation().getId();
+//    public RecipeDtoOld register(WriteRecipeRequest writeRecipeRequest, UserEntity user) {
+//        Long userId = user.getId();
+//        // Long locationId = writeRecipeRequest.getLocation().getId();
+//
+//        LocationEntity validateLocation = locationRepository.findLocationByDong(writeRecipeRequest.getLocation().getDong())
+//            .orElseThrow(() -> new AppException(ErrorCode.LOCATION_NOT_FOUND, "해당 지역이 존재하지 않습니다."));
+//
+//        UserEntity validateUser = userRepository.findById(userId)
+//            .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND, "사용자가 존재하지 않습니다."));
+//
+//        RecipeEntityOld savedRecipe = recipeRepository.save(writeRecipeRequest.toEntity(validateUser, validateLocation));
+//
+//        RecipeDtoOld savedRecipeDtoOld = RecipeDtoOld.toDto(savedRecipe);
+//
+//        return savedRecipeDtoOld;
+//    }
 
-        LocationEntity validateLocation = locationRepository.findLocationByDong(writeRecipeRequest.getLocation().getDong())
-            .orElseThrow(() -> new AppException(ErrorCode.LOCATION_NOT_FOUND, "해당 지역이 존재하지 않습니다."));
-
-        UserEntity validateUser = userRepository.findById(userId)
-            .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND, "사용자가 존재하지 않습니다."));
-
-        RecipeEntity savedRecipe = recipeRepository.save(writeRecipeRequest.toEntity(validateUser, validateLocation));
-
-        RecipeDto savedRecipeDto = RecipeDto.toDto(savedRecipe);
-
-        return savedRecipeDto;
+    // 레시피 조회
+    public Page<RecipeDto> findAllRecipes (Pageable pageable) {
+        Page<RecipeEntity> recipeEntities = recipeRepository.findAll(pageable);
+        return recipeEntities.map(recipe -> RecipeDto.toDto(recipe));
     }
 }
