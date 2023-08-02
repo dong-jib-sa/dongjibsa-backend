7c8343c28bbc5cdb44c258d0390565b3e2a82b1c
hyeonju01 <tangled6648@gmail.com>
2023-08-03 05:50:53 +0900

    feat: 조회 기능을 위한 repository 인터페이스 생성

--- src/main/java/com/djs/dongjibsabackend/repository/RecipeIngredientRepository.java
+++ src/main/java/com/djs/dongjibsabackend/repository/RecipeIngredientRepository.java
@@ -1,8 +1,11 @@
 package com.djs.dongjibsabackend.repository;
 
 import com.djs.dongjibsabackend.domain.entity.RecipeIngredientEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredientEntity, Long> {
+
+    List<RecipeIngredientEntity> findAllIngredientsByRecipeId(Long recipeId);
 
 }
