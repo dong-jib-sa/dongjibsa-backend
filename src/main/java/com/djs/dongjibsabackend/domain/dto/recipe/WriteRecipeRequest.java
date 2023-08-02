895fea19c73692f581a4a5637dc3f1cfecf80deb
hyeonju01 <tangled6648@gmail.com>
2023-08-03 05:49:57 +0900

    Refactor: 테이블 정규화에 따른 필드 변경

--- src/main/java/com/djs/dongjibsabackend/domain/dto/recipe/WriteRecipeRequest.java
+++ src/main/java/com/djs/dongjibsabackend/domain/dto/recipe/WriteRecipeRequest.java
@@ -2,12 +2,15 @@ package com.djs.dongjibsabackend.domain.dto.recipe;
 
 import com.djs.dongjibsabackend.domain.dto.ingredient.IngredientDto;
 import com.djs.dongjibsabackend.domain.dto.ingredient.IngredientResponse;
+import com.djs.dongjibsabackend.domain.dto.location.LocationDto;
+import com.djs.dongjibsabackend.domain.dto.recipe_ingredient.RecipeIngredientDto;
 import com.djs.dongjibsabackend.domain.entity.IngredientEntity;
 import com.djs.dongjibsabackend.domain.entity.LocationEntity;
 import com.djs.dongjibsabackend.domain.entity.RecipeEntity;
 import com.djs.dongjibsabackend.domain.entity.RecipeIngredientEntity;
 import com.djs.dongjibsabackend.domain.entity.UserEntity;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,29 +26,27 @@ public class WriteRecipeRequest {
     private Integer pricePerOne; // 1인 당 예상 가격
     private Integer calorie; // 1인분 칼로리
     private Integer peopleCount; // 파티원 수
-    private LocationEntity location; // 지역
-    private List<RecipeIngredientEntity> ingredientList; // 재료 (재료명, 구매 수량, 필요 수량, 나눔 수량)
+    private String dong; // 위치 dto
+    private List<RecipeIngredientDto> recipeIngredientDtos; //레시피-재료 dto 리스트 }
     private String imgUrl;
 
+
     @Builder
-    public WriteRecipeRequest(String title, String content, Integer expectingPrice, Integer pricePerOne, Integer calorie,
-                              Integer peopleCount,
-                              LocationEntity location,
-                              List<RecipeIngredientEntity> ingredientList, Integer totalQty,
-                              Integer requiredQty,
-                              Integer sharingAvailableQty, String imgUrl) {
+    public WriteRecipeRequest(String title, String content, Integer expectingPrice,
+                              Integer pricePerOne, Integer calorie,
+                              Integer peopleCount, String imgUrl) {
         this.title = title;
         this.content = content;
         this.expectingPrice = expectingPrice;
         this.pricePerOne = pricePerOne;
         this.calorie = calorie;
         this.peopleCount = peopleCount;
-        this.location = location;
-        this.ingredientList = ingredientList;
         this.imgUrl = imgUrl;
     }
 
-    public RecipeEntity toEntity(UserEntity user, LocationEntity location) {
+    public RecipeEntity toEntity(UserEntity user,
+                                 LocationEntity location,
+                                 List<RecipeIngredientEntity> recipeIngredients) {
         return RecipeEntity.builder()
                            .title(this.title)
                            .content(this.content)
@@ -55,17 +56,10 @@ public class WriteRecipeRequest {
                            .calorie(this.calorie)
                            .peopleCount(this.peopleCount)
                            .location(location)
-                           .ingredientList(this.ingredientList)
+                           .recipeIngredients(recipeIngredients)
                            .imgUrl(this.imgUrl)
                            .build();
     }
 
-//    public List<IngredientEntity> fromDtosToEntities(List<IngredientDto> ingredientDtos) {
-//
-//        return ingredientDtos.stream().map(ingredientDto -> IngredientResponse.builder()
-//            .id(ingredientDto.getId())
-//            .ingredientName(ingredientDto.getName())
-//            .build());
-//
-//    }
+
 }
