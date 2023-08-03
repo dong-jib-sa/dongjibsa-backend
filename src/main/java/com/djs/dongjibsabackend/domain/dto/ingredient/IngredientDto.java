f3cf2267085a12b0b4ad8ce64a0a12ce647b0dce
hyeonju01 <tangled6648@gmail.com>
2023-08-03 22:36:10 +0900

    Chore: 불필요한 패키지 삭제

--- src/main/java/com/djs/dongjibsabackend/domain/dto/ingredient/IngredientDto.java
+++ src/main/java/com/djs/dongjibsabackend/domain/dto/ingredient/IngredientDto.java
@@ -1,11 +1,6 @@
 package com.djs.dongjibsabackend.domain.dto.ingredient;
 
-import com.djs.dongjibsabackend.domain.dto.recipe.RecipeDto;
-import com.djs.dongjibsabackend.domain.entity.IngredientEntity;
-import com.djs.dongjibsabackend.domain.entity.RecipeEntity;
-import java.util.List;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
