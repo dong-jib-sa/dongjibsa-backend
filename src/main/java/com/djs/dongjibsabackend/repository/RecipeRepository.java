9857323afcb01252f652269154af24d77182cb77
hyeonju01 <tangled6648@gmail.com>
2023-08-03 21:01:14 +0900

    Feat: 거주 동별 레시피 리스트 조회 기능 구현을 위한 추상 메서드 작성

--- src/main/java/com/djs/dongjibsabackend/repository/RecipeRepository.java
+++ src/main/java/com/djs/dongjibsabackend/repository/RecipeRepository.java
@@ -1,8 +1,13 @@
 package com.djs.dongjibsabackend.repository;
 
 import com.djs.dongjibsabackend.domain.entity.RecipeEntity;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecipeRepository extends JpaRepository<RecipeEntity, Long> {
+
+    Page<RecipeEntity> findAllByLocationId(Pageable pageable, Long locationId);
 
 }
