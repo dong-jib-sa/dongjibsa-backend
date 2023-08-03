cc8ff7fef322ca881d078c4c4ad9a98b183e50c7
hyeonju01 <tangled6648@gmail.com>
2023-08-04 06:57:53 +0900

    Refactor: 생성자, 매서드 내 재료 이름 필드 작성

--- src/main/java/com/djs/dongjibsabackend/domain/dto/post_ingredient/PostIngredientDto.java
+++ src/main/java/com/djs/dongjibsabackend/domain/dto/post_ingredient/PostIngredientDto.java
@@ -1,6 +1,8 @@
 package com.djs.dongjibsabackend.domain.dto.post_ingredient;
 
 import com.djs.dongjibsabackend.domain.entity.PostIngredientEntity;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,28 +21,44 @@ public class PostIngredientDto {
     private Integer sharingAvailableQty;
 
     @Builder
-    public PostIngredientDto(Long id, Long postId, Long ingredientId, Integer totalQty, Integer requiredQty,
+    public PostIngredientDto(Long id, Long postId, Long ingredientId, String ingredientName, Integer totalQty, Integer requiredQty,
                              Integer sharingAvailableQty) {
         this.id = id;
         this.postId = postId;
         this.ingredientId = ingredientId;
+        this.ingredientName = ingredientName;
         this.totalQty = totalQty;
         this.requiredQty = requiredQty;
         this.sharingAvailableQty = sharingAvailableQty;
     }
 
+
+
+
     /*
     static method turning Entity to DTO
      */
-    public static PostIngredientDto toDto(PostIngredientEntity postIngredientEntity) {
+    public static PostIngredientDto of (PostIngredientEntity postIngredientEntity) {
+
         return new PostIngredientDto(
             postIngredientEntity.getId(),
             postIngredientEntity.getPost().getId(), //recipe Entity id
             postIngredientEntity.getIngredient().getId(), // Ingredient Entity id
+            postIngredientEntity.getIngredient().getIngredientName(), // Ingredient Entity name
             postIngredientEntity.getTotalQty(),
             postIngredientEntity.getRequiredQty(),
             postIngredientEntity.getSharingAvailableQty()
         );
+    }
+
+    public static List<PostIngredientDto> of (List<PostIngredientEntity> entityList) {
+        List<PostIngredientDto> dtoList = new ArrayList<>();
+
+        for (PostIngredientEntity entity: entityList) {
+            PostIngredientDto dto = PostIngredientDto.of(entity);
+            dtoList.add(dto);
+        }
+        return dtoList;
     }
 
 }
