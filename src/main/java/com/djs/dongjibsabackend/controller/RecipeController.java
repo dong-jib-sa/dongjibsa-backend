61179d3cb1974a775a34c23d2657927f3b190166
hyeonju01 <tangled6648@gmail.com>
2023-08-03 21:01:40 +0900

    Feat: 거주 동별 레시피 리스트 조회 기능 구현을 위한 GET 메서드 작성

--- src/main/java/com/djs/dongjibsabackend/controller/RecipeController.java
+++ src/main/java/com/djs/dongjibsabackend/controller/RecipeController.java
@@ -2,10 +2,18 @@ package com.djs.dongjibsabackend.controller;
 
 import com.djs.dongjibsabackend.domain.dto.Response;
 import com.djs.dongjibsabackend.domain.dto.recipe.RecipeDto;
+import com.djs.dongjibsabackend.domain.dto.recipe.RecipeResponse;
 import com.djs.dongjibsabackend.domain.dto.recipe.WriteRecipeRequest;
+import com.djs.dongjibsabackend.domain.entity.RecipeEntity;
 import com.djs.dongjibsabackend.service.RecipeService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +35,16 @@ public class RecipeController {
         RecipeDto recipeDto = recipeService.register(writeRecipeRequest);
 
         return Response.success(recipeDto);
+    }
+
+    // 2. 지역별 게시글 전체 조회
+    @GetMapping("/{DongName}")
+    public Response<Page<RecipeResponse>> getAllRecipes(
+        @PageableDefault(size = 20, sort = "id", direction = Direction.ASC) Pageable pageable,
+        @PathVariable(name = "DongName") String dongName) {
+
+        Page<RecipeDto> recipeDtoPage = recipeService.searchByLocation(pageable, dongName);
+
+        return Response.success(recipeDtoPage);
     }
 }
