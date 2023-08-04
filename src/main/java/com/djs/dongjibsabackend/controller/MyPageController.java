436d0db2db76c9e8137c37862fe455d444abcf62
hyeonju01 <tangled6648@gmail.com>
2023-08-04 19:46:08 +0900

    Feat: 마이 페이지 내 총 나눔 수량, 평균 섭취 칼로리 조회 기능 구현

--- src/main/java/com/djs/dongjibsabackend/controller/MyPageController.java
+++ src/main/java/com/djs/dongjibsabackend/controller/MyPageController.java
@@ -4,6 +4,8 @@ import com.djs.dongjibsabackend.domain.dto.Response;
 import com.djs.dongjibsabackend.domain.dto.myPage.MyIndicatorResponse;
 import com.djs.dongjibsabackend.service.MyPageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +16,8 @@ public class MyPageController {
 
     private final MyPageService myPageService;
 
-    public Response getMyIndicator(Long userId) {
+    @GetMapping("/{userId}/indicator")
+    public Response getMyIndicator(@PathVariable Long userId) {
 
         MyIndicatorResponse myIndicatorResponse = myPageService.calculate(userId);
 
--- src/main/java/com/djs/dongjibsabackend/service/MyPageService.java
+++ src/main/java/com/djs/dongjibsabackend/service/MyPageService.java
@@ -1,5 +1,7 @@
 package com.djs.dongjibsabackend.service;
 
+import static java.util.stream.Collectors.reducing;
+
 import com.djs.dongjibsabackend.domain.dto.myPage.MyIndicatorResponse;
 import com.djs.dongjibsabackend.domain.dto.post.PostDto;
 import com.djs.dongjibsabackend.domain.dto.post_ingredient.PostIngredientDto;
@@ -13,6 +15,7 @@ import com.djs.dongjibsabackend.repository.PostRepository;
 import com.djs.dongjibsabackend.repository.UserRepository;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -30,47 +33,58 @@ public class MyPageService {
     }
 
     public MyIndicatorResponse calculate(Long userId) {
-        UserEntity user = findUserById(userId);
-        UserDto userDto = UserDto.toDto(user);
+        UserEntity user = findUserById(userId); // 유저
+        UserDto userDto = UserDto.toDto(user); // 유저 -> dto
 
         // 유저가 작성한 PostList
-        List<PostDto> postDtoList = userDto.getPostDtoList(); // postList -> post -> ingredient List
-
-        // PostIdList
+        List<PostDto> postDtoList = userDto.getPostDtoList(); // [{1, [[재료, 나눔수량], [재료, 나눔수량], [재료, 나눔수량], 제목, 내용, ...},
+                                                              // {2, [[재료, 나눔수량], [재료, 나눔수량], [재료, 나눔수량], 제목, 내용, ...},
+                                                              // {3, [[재료, 나눔수량], [재료, 나눔수량], [재료, 나눔수량], 제목, 내용, ...},
+                                                              // {4, [[재료, 나눔수량], [재료, 나눔수량], [재료, 나눔수량], 제목, 내용, ...}]
+        // PostIdList : 유저가 작성한 모든 게시글(postId)
         List<Long> postIdList = new ArrayList<>();
 
         // PostDto의 칼로리 리스트, postId 리스트 생성
         List<Double> calories = new ArrayList<>();
+
         for (PostDto postDto : postDtoList) {
             double calorie = postDto.getCalorie();
-            calories.add(calorie);
-
-            postIdList.add(postDto.getId());
+            calories.add(calorie); // {칼로리1, 칼로리2, 칼로리3, ...}
+            postIdList.add(postDto.getId()); // {1, 2, 3, 4, ...}
         }
-        // 칼로리 평균 계산
+
+        // ------------------------ 칼로리 평균 계산 ------------------------
         double calorieAvg = calories.stream().mapToDouble(i -> i).average()
                                     .orElse(0);
 
-        // 총 나눔 건수
-        // 유저가 작성한 PostDto들의 postId
-
         // 유저가 작성한 Post별 재료 목록 리스트..
-        List<List<PostIngredientEntity>> postIngredientEntityList = new ArrayList<>(); // [[1, 2, 3], [4, 5, 6]]
+        List<List<PostIngredientEntity>> postIngredientEntityList = new ArrayList<>(); // { [[포스트1, 재료1, 나눔수량],
+                                                                                       //   [포스트1, 재료2, 나눔수량],
+                                                                                       //   [포스트1, 재료3, 나눔수량]],
+                                                                                       //
+                                                                                       //   [[포스트2, 재료1, 나눔수량],
+                                                                                       //   [포스트2, 재료2, 나눔수량],
+                                                                                       //   [포스트2, 재료3, 나눔수량]], ... }
         List<PostIngredientDto> postIngredientsList = new ArrayList<>();
+        List<Integer> sumOfSharingAvailableQtyPerPost = new ArrayList<>();
 
         // 게시글의 모든 재료의 총 나눔 건수의 총합
-//        for (Long postId: postIdList) {
-//            List<PostIngredientEntity> postIngredient = postIngredientRepository.findAllIngredientsByPostId(postId);
-//            Integer sumOfSharingAvailableQty = 0;
-//            postIngredient.forEach(postIngredientEntity -> sumOfSharingAvailableQty += postIngredientEntity.getSharingAvailableQty());
-//
-//
-//        }
-//        List<PostIngredientDto> postIngredients = postDto.getRecipeIngredients();
-//        postIngredientsList.add(postIngredients);
+        for (Long postId: postIdList) {
+            List<PostIngredientEntity> postIngredient = postIngredientRepository.findAllIngredientsByPostId(postId);
+            Integer sumOfSharingAvailableQty = postIngredient.stream().collect(reducing(0,
+                                                                                        PostIngredientEntity::getSharingAvailableQty,
+                                                                                        Integer::sum)); // 레시피 별 재료 나눔 건수의 총합
+            sumOfSharingAvailableQtyPerPost.add(sumOfSharingAvailableQty);
+        }
+
+        // ------------------------ 총 나눔 건수 ------------------------
+        Integer sumOfSharingAvailableQtyPerUser = sumOfSharingAvailableQtyPerPost.stream().reduce(0, (a, b) -> a+b);
 
 
 
-
+        return MyIndicatorResponse.builder()
+                                  .calorieAvg(calorieAvg)
+                                  .sumOfSharingAvailableQty(sumOfSharingAvailableQtyPerUser)
+                                  .build();
     }
 }
