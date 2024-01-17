3c7ef86e998462326550296e4f6e63f96351ad7d
hyeonju01 <tangled6648@gmail.com>
2024-01-18 03:38:19 +0900

    Feat: CustomException 작성

---
+++ src/main/java/com/djs/dongjibsabackend/exception/ExceptionManager.java
@@ -0,0 +1,23 @@
+package com.djs.dongjibsabackend.exception;
+
+import com.djs.dongjibsabackend.domain.dto.ErrorResponse;
+import com.djs.dongjibsabackend.domain.dto.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionManager {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> runTimeExceptionHandler(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                             .body("INTERNAL SERVER ERROR");
+    }
+
+    @ExceptionHandler(AppException.class)
+    public ResponseEntity<?> appExceptionHandler(AppException e) {
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+                             .body(Response.error(new ErrorResponse(e.getErrorCode().toString(), e.getErrorMessage())));
+    }
+}
