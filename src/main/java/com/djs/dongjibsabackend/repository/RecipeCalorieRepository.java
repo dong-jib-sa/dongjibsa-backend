8cd0dd217b5342646b07ab5e6f48b237ae572a27
hyeonju01 <tangled6648@gmail.com>
2024-01-19 01:08:09 +0900

    Refactor: Optional 객체로 반환하도록 수정

--- src/main/java/com/djs/dongjibsabackend/repository/RecipeCalorieRepository.java
+++ src/main/java/com/djs/dongjibsabackend/repository/RecipeCalorieRepository.java
@@ -1,10 +1,12 @@
 package com.djs.dongjibsabackend.repository;
 
 import com.djs.dongjibsabackend.domain.entity.RecipeCalorieEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecipeCalorieRepository extends JpaRepository<RecipeCalorieEntity, Long> {
 
-    RecipeCalorieEntity findByRecipeName(String recipeName);
+     Optional<RecipeCalorieEntity> findByRecipeName(String recipeName);
+//    RecipeCalorieEntity findByRecipeName(String recipeName);
 
 }
