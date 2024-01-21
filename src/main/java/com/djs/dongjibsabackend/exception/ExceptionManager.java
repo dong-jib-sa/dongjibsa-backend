1418a34893e514f5997c77b907098318b5cbb479
hyeonju01 <tangled6648@gmail.com>
2024-01-21 23:38:59 +0900

    Add: 요청 본문이 누락되었을 경우 사용되는 전역 에러 핸들러 추가

--- src/main/java/com/djs/dongjibsabackend/exception/ExceptionManager.java
+++ src/main/java/com/djs/dongjibsabackend/exception/ExceptionManager.java
@@ -2,8 +2,12 @@ package com.djs.dongjibsabackend.exception;
 
 import com.djs.dongjibsabackend.domain.dto.ErrorResponse;
 import com.djs.dongjibsabackend.domain.dto.Response;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +17,14 @@ public class ExceptionManager {
     public ResponseEntity<?> runTimeExceptionHandler(RuntimeException e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                              .body("INTERNAL SERVER ERROR");
+    }
+
+    // @Order(Ordered.HIGHEST_PRECEDENCE)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> notReadableExceptionHandler(HttpMessageNotReadableException e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                             .body("요청 본문이 필요합니다.");
     }
 
     @ExceptionHandler(AppException.class)
